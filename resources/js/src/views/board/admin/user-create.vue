5c6b8c9054528c15830143a1acd53d70e659d398
Chul <ct@ibugroup.org>
2022-12-20 17:17:58 +0800

    ui

--- resources/js/src/views/board/admin/user-create.vue
+++ resources/js/src/views/board/admin/user-create.vue
@@ -1,5 +1,15 @@
+
+
 <template>
-    <div>
-        geel
-    </div>
+   <v-col
+      cols="12"
+      md="6"
+    >
+      <v-card>
+        <v-card-title>Horizontal Form with Icons</v-card-title>
+        <v-card-text>
+          <demo-form-layout-horizontal-icon></demo-form-layout-horizontal-icon>
+        </v-card-text>
+      </v-card>
+    </v-col>
 </template>
